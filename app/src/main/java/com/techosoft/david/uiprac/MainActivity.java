a1376f765d5fe00f9b4ec6db9b65377fc993c3b8
david <yiyashao@gmail.com>
2016-08-26 14:10:05 +0800

    Initial commit

---
+++ app/src/main/java/com/techosoft/david/uiprac/MainActivity.java
@@ -0,0 +1,25 @@
+package com.techosoft.david.uiprac;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+        View view = getSupportActionBar().getCustomView();
+
+       // ImageButton imageButton = (ImageButton)view.findViewById(R.id.action_bar_back);
+
+
+    }
+}
